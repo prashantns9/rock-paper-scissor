60dd161cf140bf4a8517b5caecb6d25a5659ede7
Prashant Shinde <prashantns9@gmail.com>
2021-01-01 12:53:23 +0530

    improved stability

--- rps-server/app/rps.js
+++ rps-server/app/rps.js
@@ -1,50 +1,65 @@
 var rooms = [];
 
 function doesRoomExists(roomName) {
-    let roomExists = rooms.find((room) => {
-        return (room.name === roomName);
-    });
-    return roomExists;
+  let roomExists = rooms.find((room) => {
+    return room.name === roomName;
+  });
+  return roomExists;
 }
 
 var getRoom = (roomName) => {
-    let roomToReturn = rooms.filter(room => room.name === roomName);
-    if(roomToReturn.length === 1){
-        return roomToReturn[0];
-    }else{
-        return null;
-    }
-}
-
+  let roomToReturn = rooms.filter((room) => room.name === roomName);
+  if (roomToReturn.length === 1) {
+    return roomToReturn[0];
+  } else {
+    return null;
+  }
+};
 
 var createRoom = (roomName) => {
-    if (roomName && !doesRoomExists(roomName)) {
-        let newRoom = {
-            name: roomName,
-            size: 0
-        }
-        rooms.push(newRoom);
-        return true;
-    } else {
-        return false;
-    }
-}
+  if (roomName && !doesRoomExists(roomName)) {
+    let newRoom = {
+      name: roomName,
+      size: 0,
+    };
+    rooms.push(newRoom);
+    return true;
+  } else {
+    return false;
+  }
+};
 
-var joinRoom = (socket,roomName) => {
-    let roomJoined = false;
-    rooms.forEach(room => {
-        if ((room.name === roomName) && room.size < 3) {
-            socket.join(roomName);
-            room.size += 1;
-            roomJoined = true;
-        }
-    });
-    return roomJoined;
-}
+var joinRoom = (socket, roomName) => {
+  let roomJoined = false;
+  rooms.forEach((room) => {
+    if (room.name === roomName && room.size < 2) {
+      socket.join(roomName);
+      room.size += 1;
+      roomJoined = true;
+    }
+  });
+  return roomJoined;
+};
 
 var startGame = (socket, roomName) => {
-    socket.to(roomName).emit('start-game');
-    socket.emit('start-game');
-}
+  socket.to(roomName).emit("start-game");
+  socket.emit("start-game");
+};
 
-module.exports = { rooms, createRoom, joinRoom, getRoom, startGame };
+var leaveRooms = (socket) => {
+  let roomsToLeave = Object.keys(socket.rooms);
+
+  roomsToLeave.forEach((room) => {
+    socket.to(room).emit("opponent-left");
+    let roomIndex = rooms.findIndex((r) => r.name === room);
+    rooms.splice(roomIndex, 1);
+  });
+};
+module.exports = {
+  rooms,
+  createRoom,
+  joinRoom,
+  getRoom,
+  startGame,
+  leaveRooms,
+};
